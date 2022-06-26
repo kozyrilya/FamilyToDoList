5dd1dee7a361d19b6bb987f786fd74576e745833
Kozyr Illia <illia.kozyr@macys.com>
2022-06-26 14:01:23 +0300

    Setup the project. Removed storyboards from the project.

---
+++ FamilyToDoList/FamilyToDoList/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FamilyToDoList
+//
+//  Created by Illia Kozyr on 6/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+    }
+}
+
